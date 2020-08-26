f793c43d17a4a26a578c3d4265d68a6956035742
unknown <brownnguyen268@gmail.com>
2020-08-26 16:43:48 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
-window.onload = function () {
-    let arrowLeft = document.querySelector('.staff-content .slick-next');
-    arrowLeft.innerHTML = '<span class="arrow-right"></span>'
-    let arrowRight = document.querySelector('.staff-content .slick-prev');
-    arrowRight.innerHTML = '<span class="arrow-left"></span>'
-}
-
-
 
 $(document).ready(function () {
     $('.top-slide').slick({
         arrows: false
     });
-});
-$(document).ready(function () {
     $('.staff-content').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -35,4 +25,36 @@ $(document).ready(function () {
             }
         ]
     });
+    let arrowLeft = document.querySelector('.staff-content .slick-next');
+    arrowLeft.innerHTML = '<span class="arrow-right"></span>'
+    let arrowRight = document.querySelector('.staff-content .slick-prev');
+    arrowRight.innerHTML = '<span class="arrow-left"></span>'
 });
+
+let menuBurger = document.querySelector(".menu-burger .menu");
+let drawer = document.querySelector('.drawer');
+menuBurger.onclick = function () {
+    menuBurger.classList.toggle('active');
+    if (menuBurger.classList.contains('active')) {
+        drawer.classList.add('active');
+    }
+    else {
+        drawer.classList.remove('active');
+    }
+}
+$('#backTop').click(function (event) {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000, 'swing');
+    return false;
+});
+window.onscroll = function () {
+    if (window.pageYOffset > window.innerHeight / 2) {
+        document.querySelector('#backTop').style.opacity = 0.8;
+        document.querySelector('#backTop').style.visibility = "visible"
+    }
+    else {
+        document.querySelector('#backTop').style.opacity = 0;
+        document.querySelector('#backTop').style.visibility = "hidden"
+    }
+}
